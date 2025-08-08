c1a8a71b2806f3700ca3da2ca1165b0abad39c31
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-08-08 17:41:08 -0300

    feat: iitialize project with TanStack Router and basic boilerplate files

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vitest/config";
+import viteReact from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
