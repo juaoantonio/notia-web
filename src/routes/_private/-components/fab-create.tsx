6a78985902ec8ea572843174787a1334fcc0490f
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-08-22 18:31:29 -0300

    fix: adjust `fab-create` button dimensions for improved consistency

--- src/routes/_private/-components/fab-create.tsx
+++ src/routes/_private/-components/fab-create.tsx
@@ -6,11 +6,10 @@ export function FabCreate() {
   return (
     <div className="pointer-events-none fixed inset-x-0 bottom-20 z-30 flex justify-end pr-5">
       <Button
-        size="icon"
-        className="pointer-events-auto rounded-full shadow-sm"
+        className="pointer-events-auto h-15 w-15 rounded-full shadow-sm"
         aria-label="Add item"
       >
-        <Plus className="h-5 w-5" />
+        <Plus className="min-h-6 min-w-6" />
       </Button>
     </div>
   );
